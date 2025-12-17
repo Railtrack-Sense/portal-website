aa148911e3dd20b70c9543336cca02794ca05be5
MacroMelon <earwax28@gmail.com>
2025-12-18 02:37:15 +0530

    Remove some template stuff

--- src/routes/_headerFooterLayout.tsx
+++ src/routes/_headerFooterLayout.tsx
@@ -20,7 +20,6 @@ function LayoutComponent() {
                     <div className="ml-10 shrink-0">
                         <img src={icon} alt="logo" className="w-12 h-12" />
                     </div>
-                    <div className="text-3xl md:max-w-screen max-w-0 ml-5 pt-1 font-bold text-white">Streamline</div>
                 </a>
 
             </header>
@@ -33,39 +32,7 @@ function LayoutComponent() {
             <footer className="bg-side-color py-8 mt-auto">
                 <div className="container mx-auto px-4">
                     <div className="flex flex-col md:flex-row justify-between items-center">
-                        <div className="flex items-center space-x-2 mb-4 md:mb-0">
-                            <img src={icon} alt="logo" className="w-6 h-6" />
-                            <span className="text-xl text-white font-bold">Streamline</span>
-                        </div>
-                        <div className="flex space-x-6 font-medium text-blue-400">
-                            <a
-                                className="hover:text-blue-300"
-                                href="/about"
-                            >
-                                About
-                            </a>
-                            <a
-                                className="hover:text-blue-300"
-                                href="mailto:contact@streamlineaddons.com"
-                            >
-                                Email
-                            </a>
-                            <a
-                                className="hover:text-blue-300"
-                                href="https://www.facebook.com/people/StreamlineApp/61572840522852/"
-                            >
-                                Facebook
-                            </a>
-                            <a
-                                className="hover:text-blue-300"
-                                href="https://discord.gg/zJGEyPNugg"
-                            >
-                                Discord
-                            </a>
-                            <a href="/eula" className="hover:text-blue-300">
-                                EULA
-                            </a>
-                        </div>
+        
                     </div>
                 </div>
             </footer>
